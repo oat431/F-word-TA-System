832df772229a99aa16a5470e55376aab6a346204
oat431 <oat431@gmail.com>
2021-12-30 19:28:59 +0700

    [👌 setting up]

--- src/routes.js
+++ src/routes.js
@@ -9,10 +9,6 @@ export const routes = [
     path: '/about',
     meta: { title: 'About' },
     component: About,
-    // example of route level code-splitting
-    // this generates a separate chunk (About.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    // component: () => import('./views/About.vue')
   },
   { path: '/:path(.*)', component: NotFound },
 ]
